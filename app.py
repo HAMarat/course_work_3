299ca67dacb7bb8d9d3d5f8ddbd66645341a32cd
Huawei_station <smile56680@gmail.com>
2022-11-11 14:32:48 +0300

    home 2

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template
-from utils import get_posts_all, get_post_content, get_posts_by_user
+from flask import Flask, render_template, request
+from utils import get_posts_all, get_post_content, get_posts_by_user, get_comments_by_post_id
 
 app = Flask(__name__)
 
@@ -10,10 +10,18 @@ def main_page():
     return render_template('index.html', posts=posts)
 
 
-@app.route('/posts/<post_id>')
+@app.route('/posts/<int:post_id>')
 def post_by_post_id(post_id):
     post = get_post_content(post_id)
-    return render_template('post.html', post=post)
+    comments = get_comments_by_post_id(post_id)
+    return render_template('post.html', post=post, comments=comments)
 
 
-app.run(debug=True, host='127.0.0.1', port=2000)
+@app.route('/search', methods=['GET'])
+def search():
+    s = request.args['s']
+    print(s)
+    return render_template('search.html', s=s)
+
+
+app.run(debug=True, host='127.0.0.1', port=3000)
--- utils.py
+++ utils.py
@@ -1,7 +1,7 @@
 import json
 
 POST_PATH = "static/data/posts.json"
-
+COMMENTS_PATH = "static/data/comments.json"
 
 def get_posts_all():
     with open(POST_PATH, 'r', encoding='utf-8') as file:
@@ -17,10 +17,19 @@ def get_posts_by_user(user_name):
     return user_posts
 
 
+def get_comments_all():
+    with open(COMMENTS_PATH, 'r', encoding='utf-8') as file:
+        all_comments = json.load(file)
+        return all_comments
+
+
 def get_comments_by_post_id(post_id):
-    for post in get_posts_all():
-        if post["pk"] == post_id:
-            return post["content"]
+    comments = []
+    for comment in get_comments_all():
+        if comment["post_id"] == post_id:
+            comments.append(comment)
+    if comments:
+        return comments
     return ValueError
 
 
@@ -35,4 +44,4 @@ def search_for_posts(query):
 def get_post_content(pk):
     for post in get_posts_all():
         if post["pk"] == pk:
-            return post["content"]
+            return post
