fcd77eb9ccecd6949e51c7001f200092dc4aebc2
HAMarat <smile56680@gmail.com>
2023-03-11 18:06:35 +0300

    feat: add unit fail

--- api_request/api_request.py
+++ api_request/api_request.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, jsonify
-import logging
+# import logging
 from utils import get_json_data, get_post_by_post_id, POST_PATH
 
 # создаем Blueprint для работы с API
-api_request = Blueprint('apip_request', __name__)
+api_request = Blueprint('api_request', __name__)
 
 #logging.basicConfig(filename='logs/api.log', level=logging.INFO, encoding='utf-8',
 #                    format="%(asctime)s %(levelname)s %(message)s")
@@ -14,7 +14,7 @@ def post_by_api():
     """
     Представление обрабатывающее GET запрос и возвращающее данные постов в json формате
     """
-    logging.info('Произведен запрос к /api/posts')
+    # logging.info('Произведен запрос к /api/posts')
     posts = get_json_data(POST_PATH)
     return jsonify(posts)
 
@@ -24,6 +24,6 @@ def get_api_post(post_id):
     """
     Представление обрабатывающее GET запрос и возвращающее данные поста в json формате
     """
-    logging.info(f'Произведен запрос к /api/posts/{post_id}')
+    # logging.info(f'Произведен запрос к /api/posts/{post_id}')
     post = get_post_by_post_id(post_id)
     return jsonify(post)
